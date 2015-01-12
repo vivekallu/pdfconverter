54f0940e545e18ed2c94d29a53ca42938aac790b
vivekallu <allu.vivek7@gmail.com>
2015-01-12 17:02:56 +0530

    basic tests for form and status code in httpresponse

--- pdfconverter/application/tests.py
+++ pdfconverter/application/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .form import UploadFileForm
+from .views import pdf_converter
 
-# Create your tests here.
+class url_FormTestCase(TestCase):
+    ''' Basic tests to check the form validity'''
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+
+    def test_valid_form(self):
+        data = {'url':'http://www.google.com'}
+        form = UploadFileForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        data = {
+            'url': 'Some fake url',
+        }
+        form = UploadFileForm(data=data)
+        self.assertFalse(form.is_valid())
+   
+    def test_getmethod(self):
+        request = self.factory.get('/')
+        response = pdf_converter(request)
+        self.assertEqual(response.status_code, 200)
+  
+
+    def test_postmethod(self):
+        request = self.factory.post('/', {"url": "http://cricinfo.com"})
+        response = pdf_converter(request)
+        self.assertEqual(response.status_code, 200)
+        
